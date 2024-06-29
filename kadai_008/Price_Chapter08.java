53c557ffa1ca789951978b67b0443e4c1747b2f0
sjyoseishi <yiting19841104@gmail.com>
2024-06-29 18:09:55 +0900

    kadai_008を修正

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -4,36 +4,44 @@ public class Price_Chapter08 {
 
 	public static void main(String[] args) {
 		//サービスの使用料金を表す変数
-		int serviceCost [] = {1000,2000,3000,4000,5000,500};
+		int serviceCost;
         //年代を表す変数
         int userAge = 30;
         
         switch(userAge/10) {
         	case 1  -> {
-        		System.out.println(userAge + "代の料金は" + serviceCost[0] +  "円");
+        		serviceCost = 1000;
+        		System.out.println(userAge + "代の料金は" + serviceCost +  "円");
         	}
         	case 2  -> {
-        		System.out.println(userAge + "代の料金は" + serviceCost[1] +  "円");
+        		serviceCost = 2000;
+        		System.out.println(userAge + "代の料金は" + serviceCost +  "円");
         	}
         	case 3  -> {
-        		System.out.println(userAge + "代の料金は" + serviceCost[2] +  "円");
+        		serviceCost = 3000;
+        		System.out.println(userAge + "代の料金は" + serviceCost +  "円");
         	}
         	case 4  -> {
-        		System.out.println(userAge + "代の料金は" + serviceCost[2] +  "円");
+        		serviceCost = 3000;
+        		System.out.println(userAge + "代の料金は" + serviceCost +  "円");
         	}
         	case 5  -> {
-        		System.out.println(userAge + "代の料金は" + serviceCost[3] +  "円");
+        		serviceCost = 4000;
+        		System.out.println(userAge + "代の料金は" + serviceCost +  "円");
         	}
         	case 6  -> {
-        		System.out.println(userAge + "代の料金は" + serviceCost[3] +  "円");
+        		serviceCost = 4000;
+        		System.out.println(userAge + "代の料金は" + serviceCost +  "円");
         	}
         	case 7  -> {
-        		System.out.println(userAge + "代の料金は" + serviceCost[3] +  "円");
+        		serviceCost = 4000;
+        		System.out.println(userAge + "代の料金は" + serviceCost +  "円");
         	}
         	case 8  -> {
-        		System.out.println(userAge + "代の料金は" + serviceCost[4] +  "円");
+        		serviceCost = 5000;
+        		System.out.println(userAge + "代の料金は" + serviceCost +  "円");
         	}
-        	default   -> System.out.println("上記以外の年代" + serviceCost[5] +  "円");
+        	default   -> System.out.println("上記以外の年代の料金は500円");
         }
 	}
 }
