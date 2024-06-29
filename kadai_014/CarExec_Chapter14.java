63d3595f9a8195605e6164650904400fdd6f5040
sjyoseishi <yiting19841104@gmail.com>
2024-06-30 00:29:45 +0900

    kadai_014を修正

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -1,36 +1,14 @@
 package kadai_014;
 
-//車クラス
 public class CarExec_Chapter14 {
 
-	//フィールド（内部データ）
-	private int gear = 1; //ギアの初期値
-	private int speed = 10; //速度の初期値
+	public static void main(String[] args) {
+		//車クラスのインスタンスを作成する
+		Car_Chapter14 gear = new Car_Chapter14();
 
-	 // コンストラクタ
-	public CarExec_Chapter14()  {
-		
+		//車クラスのgearChangeメソッドを実行する
+		gear.gearChange(3);
+		//車クラスのrunメソッドを実行する
+		gear.run();
 	}
-	
-	//メソッド
-	//ギアの値により速度を変える
-	public void gearChange(final int afterGear) {
-		this.gear = afterGear;
-		//ギアの速度
-		switch (this.gear) {
-			case 1 -> this.speed = 10;
-			case 2 -> this.speed = 20;
-			case 3 -> this.speed = 30;
-			case 4 -> this.speed = 40;
-			case 5 -> this.speed = 50;
-			default -> this.speed = 10;
-		}
-		
-	}
-	//ギアチェンジ後の速度を表示する	
-	public void run() {
-		System.out.println("ギア1から" + this.gear + "に切り替えました" );
-		System.out.println("速度は時速" + this.speed + "kmです" );
-	}
-	
 }
--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -1,14 +1,36 @@
 package kadai_014;
 
+//車クラス
 public class Car_Chapter14 {
 
-	public static void main(String[] args) {
-		//車クラスのインスタンスを作成する
-		CarExec_Chapter14 gear = new CarExec_Chapter14();
+	//フィールド（内部データ）
+	private int gear = 1; //ギアの初期値
+	private int speed = 10; //速度の初期値
 
-		//車クラスのgearChangeメソッドを実行する
-		gear.gearChange(4);
-		//車クラスのrunメソッドを実行する
-		gear.run();
+	 // コンストラクタ
+	public Car_Chapter14()  {
+		
 	}
+	
+	//メソッド
+	//ギアの値により速度を変える
+	public void gearChange(final int afterGear) {
+		this.gear = afterGear;
+		//ギアの速度
+		switch (this.gear) {
+			case 1 -> this.speed = 10;
+			case 2 -> this.speed = 20;
+			case 3 -> this.speed = 30;
+			case 4 -> this.speed = 40;
+			case 5 -> this.speed = 50;
+			default -> this.speed = 10;
+		}
+		
+	}
+	//ギアチェンジ後の速度を表示する	
+	public void run() {
+		System.out.println("ギア1から" + this.gear + "に切り替えました" );
+		System.out.println("速度は時速" + this.speed + "kmです" );
+	}
+	
 }
